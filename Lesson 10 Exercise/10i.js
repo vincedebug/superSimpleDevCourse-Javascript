49b1e590d63ef12cd3d5f943261e837909793878
vincedebug <vinceferrerasermino25@gmail.com>
2024-01-28 16:29:33 +0800

    separate the js and css code

---
+++ Lesson 10 Exercise/10i.js
@@ -0,0 +1,21 @@
+var calculation = '';
+
+        function updateCalculation(add) {
+            
+            var dispay = document.querySelector('.textOutput')
+            dispay.textContent += add;
+            calculation += add;
+
+        }
+
+        function result() {
+            var dispay = document.querySelector('.textOutput')
+            dispay.textContent = eval(calculation);
+            localStorage.setItem(calculation, eval(calculation));
+        }
+
+        function remove() {
+            var display = document.querySelector('.textOutput')
+            calculation = '';
+            display.textContent = '';
+        }
