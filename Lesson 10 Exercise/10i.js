535a3dd527c43249887ecaa401a753e77339af32
vincedebug <vinceferrerasermino25@gmail.com>
2024-01-28 16:48:42 +0800

    changed the class names

--- Lesson 10 Exercise/10i.js
+++ Lesson 10 Exercise/10i.js
@@ -1,21 +1,19 @@
 var calculation = '';
+    function updateCalculation(add) {
+        
+        var dispay = document.querySelector('.textOutput')
+        dispay.textContent += add;
+        calculation += add;
+    }
 
-        function updateCalculation(add) {
-            
-            var dispay = document.querySelector('.textOutput')
-            dispay.textContent += add;
-            calculation += add;
+    function result() {
+        var dispay = document.querySelector('.textOutput')
+        dispay.textContent = eval(calculation);
+        localStorage.setItem(calculation, eval(calculation));
+    }
 
-        }
-
-        function result() {
-            var dispay = document.querySelector('.textOutput')
-            dispay.textContent = eval(calculation);
-            localStorage.setItem(calculation, eval(calculation));
-        }
-
-        function remove() {
-            var display = document.querySelector('.textOutput')
-            calculation = '';
-            display.textContent = '';
-        }
+    function clearText() {
+        var display = document.querySelector('.textOutput')
+        calculation = '';
+        display.textContent = '';
+    }
